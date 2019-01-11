f1f9fce9c32680fe628f8310cec8f664ae10e39d
Shen Ni <shenni119@gmail.com>
2019-01-10 19:38:18 -0500

    refactored code

--- fuzzywuzzy_merge_github.py
+++ fuzzywuzzy_merge_github.py
@@ -6,51 +6,53 @@ import string
 from fuzzywuzzy import fuzz
 import difflib
 
-# output_customer_aging_combined_noemailphone='output_customer_aging_combined_noemailphone.csv'
-matchfile1='output_customer_aging_combined_withemailphone2.csv'
-matchfile2='checkpoint_permit_license_combined.csv'
-# input_dept_budgetcode='budgetcode_key.csv'
+"""Fuzzy matching helper function,
+Input 1: csv file with a field you want to fuzzy match to
+Input 2: another csv file with a corresponding field
+Output: csv with the 2 fuzzy-matched fields
+Prior to using this function, need to: 
+1) define directory location of both csv's,
+2) lowercase and remove special characters and punctuations from both fields"""
 
+matchfile1='csv1.csv'
+matchfile1_field_name='NAMELAST'
+matchfile2='csv2.csv'
+matchfile2_field_name='NAMELAST'
 matchfile1_df = pd.read_csv(matchfile1)
 matchfile2_df=pd.read_csv(matchfile2)
 
-permit_license_df10000=matchfile1_df.tail(10000)
-aging_df100=matchfile2_df.head(200)
+#FOR TESTING PURPOSE ONLY, IN CASE YOU WANT TO SHRINK matchfile1 OR matchfile2
+# matchfile1_df=matchfile1_df.tail(10000)
+# matchfile2_df=matchfile2_df.head(200)
 
-permit_license_df10000_notnull=permit_license_df10000[pd.notnull(permit_license_df10000.NAMELAST)]
-aging_df100_notnull=aging_df100[pd.notnull(aging_df100.NAMELAST)]
+matchfile1_df_notnull=matchfile1_df[pd.notnull(matchfile1_df[matchfile1_field_name])]
+matchfile2_df_notnull=matchfile2_df[pd.notnull(matchfile2_df[matchfile2_field_name])]
 
-permit_license_df10000_notnull.to_csv('dataframe1.csv',header=True,encoding='utf-8')
-aging_df100_notnull.to_csv('dataframe2.csv',header=True,encoding='utf-8')
-
-# headers_universal=['APKEY',"APNO",'DESCRIPT','FEEDESC','BUDGCODE','Customer ID','ADDDTTM','NAMELAST']
-# hansen_specific_headers=['Hansen_APKEY',"Hansen_APNO",'Hansen_DESCRIPT','Hansen_FEEDESC','Hansen_BUDGCODE','Hansen_Customer ID','Hansen_ADDDTTM','Hansen_NAMELAST']
-
-def get_permit_licenses(row):
-    d = permit_license_df10000_notnull.apply(lambda x: fuzz.token_set_ratio(x['NAMELAST'], row['NAMELAST']), axis=1)
-    d = d[d >= 90]
-    d = d[d < 100]
+def fuzzy_match_df(row, accuracy_min,accuracy_max, df_1):
+    d = df_1.apply(\
+        lambda x: fuzz.token_set_ratio(x[matchfile1_field_name], \
+        row[matchfile2_field_name]), axis=1)
+    d = d[d >= accuracy_min]
+    d = d[d < accuracy_max]
     return d
 
-initial_match=aging_df100_notnull.apply(get_permit_licenses, axis=1)
+initial_match=matchfile2_df_notnull.apply(\
+    lambda x: fuzzy_match_df(x, accuracy_min=90, accuracy_max=100,\
+    df_1=matchfile1_df), axis=1)
+
+#reformat output from fuzzywuzzy package
 reindex_initial_match=initial_match.stack().reset_index().rename(
     columns={"level_0": 'index_2', "level_1": 'index_1', "0": 'score'}
 ).astype({'index_1': np.int64})
 
 reindex_initial_match_merged = reindex_initial_match.merge(
-    permit_license_df10000_notnull,
+    matchfile1_df_notnull,
     left_on='index_1',
     right_index=True
 ).merge(
-    aging_df100_notnull,
+    matchfile2_df_notnull,
     left_on='index_2',
     right_index=True
 )
 
-reindex_initial_match_merged.to_csv('matched_test4.csv',header=True,encoding='utf-8')
-
-# testing3=aging_df100_notnull.apply(get_permit_licenses, axis=1)
-# testing3.to_csv('testing3.csv',header=True,encoding='utf-8')
-
-# fuzzycombine=pd.concat((aging_df100_notnull, aging_df100_notnull.apply(get_permit_licenses, axis=1)), axis=1)
-# fuzzycombine.to_csv('fuzzycombine.csv',header=True,encoding='utf-8')
+reindex_initial_match_merged.to_csv('matched.csv',header=True,encoding='utf-8')
